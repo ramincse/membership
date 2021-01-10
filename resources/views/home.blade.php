45a56d917f4944983f743ede365974ef95ca2f7e
ramincse <ramin.cse2009@gmail.com>
2021-01-10 09:11:22 +0600

    Show user profile data

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,13 +8,28 @@
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    <div class="profile">
+                        <img src="media/photos/users/{{ Auth::user() -> photo }}" alt="">
+                        <h2 class="mt-3">{{ Auth::user() -> name }}</h2>
+                        <table class="table table-striped">
+                            <tr>
+                                <td>Name</td>
+                                <td>{{ Auth::user() -> name }}</td>
+                            </tr>
+                            <tr>
+                                <td>Username</td>
+                                <td>{{ Auth::user() -> uname }}</td>
+                            </tr>
+                            <tr>
+                                <td>Email</td>
+                                <td>{{ Auth::user() -> email }}</td>
+                            </tr>
+                            <tr>
+                                <td>Cell</td>
+                                <td>{{ Auth::user() -> cell }}</td>
+                            </tr>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
