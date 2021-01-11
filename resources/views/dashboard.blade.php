d712540014637b3c6d6048492d3bb3ce8514bdd4
ramincse <ramin.cse2009@gmail.com>
2021-01-11 11:26:59 +0600

    Dashboard user control dane

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,15 +1,21 @@
 @extends('layouts.app')
-
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <ul class="list-group">
+                @if ( in_array( 'User', json_decode(Auth::user() -> role -> permission) ) )
                 <li class="list-group-item"><a href="{{ route('user.index') }}">Users</a></li>
+                @endif
+                @if ( in_array( 'Role', json_decode(Auth::user() -> role -> permission) ) )
                 <li class="list-group-item"><a href="{{ route('role.index') }}">Role</a></li>
-                <li class="list-group-item"><a href="">Add new role</a></li>
+                @endif
+                @if ( in_array( 'Settings', json_decode(Auth::user() -> role -> permission) ) )
                 <li class="list-group-item"><a href="">Settings</a></li>
+                @endif
+                @if ( in_array( 'SEO', json_decode(Auth::user() -> role -> permission) ) )
                 <li class="list-group-item"><a href="">SEO</a></li>
+                @endif
             </ul>
         </div>
     </div>
